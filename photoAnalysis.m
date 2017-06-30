3019dab4623b119c4ffdea511bd95df350142b81
recherney <recherney@gmail.com>
2017-06-30 16:14:55 -0400

    updated photo analysis because my old computer crashed

--- photoAnalysis.m
+++ photoAnalysis.m
@@ -1,39 +1,39 @@
-function varargout = untitled(varargin)
-% UNTITLED MATLAB code for untitled.fig
-%      UNTITLED, by itself, creates a new UNTITLED or raises the existing
+function varargout = photoAnalysis(varargin)
+%PHOTOANALYSIS MATLAB code file for photoAnalysis.fig
+%      PHOTOANALYSIS, by itself, creates a new PHOTOANALYSIS or raises the existing
 %      singleton*.
 %
-%      H = UNTITLED returns the handle to a new UNTITLED or the handle to
+%      H = PHOTOANALYSIS returns the handle to a new PHOTOANALYSIS or the handle to
 %      the existing singleton*.
 %
-%      UNTITLED('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in UNTITLED.M with the given input arguments.
+%      PHOTOANALYSIS('Property','Value',...) creates a new PHOTOANALYSIS using the
+%      given property value pairs. Unrecognized properties are passed via
+%      varargin to photoAnalysis_OpeningFcn.  This calling syntax produces a
+%      warning when there is an existing singleton*.
 %
-%      UNTITLED('Property','Value',...) creates a new UNTITLED or raises the
-%      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before untitled_OpeningFcn gets called.  An
-%      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to untitled_OpeningFcn via varargin.
+%      PHOTOANALYSIS('CALLBACK') and PHOTOANALYSIS('CALLBACK',hObject,...) call the
+%      local function named CALLBACK in PHOTOANALYSIS.M with the given input
+%      arguments.
 %
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help untitled
+% Edit the above text to modify the response to help photoAnalysis
 
-% Last Modified by GUIDE v2.5 27-Jun-2017 11:34:25
+% Last Modified by GUIDE v2.5 30-Jun-2017 16:01:08
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @untitled_OpeningFcn, ...
-                   'gui_OutputFcn',  @untitled_OutputFcn, ...
-                   'gui_LayoutFcn',  [] , ...
+                   'gui_OpeningFcn', @photoAnalysis_OpeningFcn, ...
+                   'gui_OutputFcn',  @photoAnalysis_OutputFcn, ...
+                   'gui_LayoutFcn',  [], ...
                    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
-    gui_State.gui_Callback = str2func(varargin{1});
+   gui_State.gui_Callback = str2func(varargin{1});
 end
 
 if nargout
@@ -44,26 +44,27 @@ end
 % End initialization code - DO NOT EDIT
 
 
-% --- Executes just before untitled is made visible.
-function untitled_OpeningFcn(hObject, eventdata, handles, varargin)
+% --- Executes just before photoAnalysis is made visible.
+function photoAnalysis_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to untitled (see VARARGIN)
+% varargin   unrecognized PropertyName/PropertyValue pairs from the
+%            command line (see VARARGIN)
 
-% Choose default command line output for untitled
+% Choose default command line output for photoAnalysis
 handles.output = hObject;
 
 % Update handles structure
 guidata(hObject, handles);
 
-% UIWAIT makes untitled wait for user response (see UIRESUME)
+% UIWAIT makes photoAnalysis wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = untitled_OutputFcn(hObject, eventdata, handles) 
+function varargout = photoAnalysis_OutputFcn(hObject, eventdata, handles)
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -71,15 +72,6 @@ function varargout = untitled_OutputFcn(hObject, eventdata, handles)
 
 % Get default command line output from handles structure
 varargout{1} = handles.output;
-
-
-% --- Executes on button press in importFile.
-function importFile_Callback(hObject, eventdata, handles)
-       bfopen
-       import()
-% hObject    handle to importFile (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 
 
 % --- Executes on slider movement.
@@ -104,23 +96,20 @@ if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColo
 end
 
 
-% --- Executes on button press in GFP.
-function GFP_Callback(hObject, eventdata, handles)
-    
-% hObject    handle to GFP (see GCBO)
+% --- Executes on button press in selectCell.
+function selectCell_Callback(hObject, eventdata, handles)
+% hObject    handle to selectCell (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% Hint: get(hObject,'Value') returns toggle state of GFP
 
-
-% --- Executes on button press in togglebutton3.
-function togglebutton3_Callback(hObject, eventdata, handles)
-% hObject    handle to togglebutton3 (see GCBO)
+% --- Executes on button press in trans.
+function trans_Callback(hObject, eventdata, handles)
+% hObject    handle to trans (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% Hint: get(hObject,'Value') returns toggle state of togglebutton3
+% Hint: get(hObject,'Value') returns toggle state of trans
 
 
 % --- Executes on button press in RFP.
@@ -132,8 +121,66 @@ function RFP_Callback(hObject, eventdata, handles)
 % Hint: get(hObject,'Value') returns toggle state of RFP
 
 
-% --- Executes on button press in selectCell.
-function selectCell_Callback(hObject, eventdata, handles)
-% hObject    handle to selectCell (see GCBO)
+% --- Executes on button press in GFP.
+function GFP_Callback(hObject, eventdata, handles)
+% hObject    handle to GFP (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of GFP
+
+
+% --- If Enable == 'on', executes on mouse press in 5 pixel border.
+% --- Otherwise, executes on mouse press in 5 pixel border or over trans.
+function trans_ButtonDownFcn(hObject, eventdata, handles)
+% hObject    handle to trans (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- If Enable == 'on', executes on mouse press in 5 pixel border.
+% --- Otherwise, executes on mouse press in 5 pixel border or over RFP.
+function RFP_ButtonDownFcn(hObject, eventdata, handles)
+% hObject    handle to RFP (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- If Enable == 'on', executes on mouse press in 5 pixel border.
+% --- Otherwise, executes on mouse press in 5 pixel border or over GFP.
+function GFP_ButtonDownFcn(hObject, eventdata, handles)
+% hObject    handle to GFP (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on key press with focus on trans and none of its controls.
+
+% hObject    handle to trans (see GCBO)
+% eventdata  structure with the following fields (see MATLAB.UI.CONTROL.UICONTROL)
+%	Key: name of the key that was pressed, in lower case
+%	Character: character interpretation of the key(s) that was pressed
+%	Modifier: name(s) of the modifier key(s) (i.e., control, shift) pressed
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on slider movement.
+function timepoint_Callback(hObject, eventdata, handles)
+% hObject    handle to timepoint (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+
+
+% --- Executes during object creation, after setting all properties.
+function timepoint_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to timepoint (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
